f1c3ddabbb2cff34fe6af34348c54279cffd7fb6
Hailey Murphy <foxfireevee@gmail.com>
2024-05-19 01:22:18 -0400

    Got the API rendering, along with the names of characters within the DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,43 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+  // State
+  const [peopleData, setPeopleData] = useState([]);
+  const [planetsData, setPlanetsData] = useState([]);
+
+  // Fetching API
+  useEffect(() => {
+    function getAPI() {
+      const people = axios.get(urlPeople)
+      .then(res => {
+        // console.log(res.data);
+        setPeopleData(res.data);
+      })
+      .catch(err => console.err(err));
+
+      const planet = axios.get(urlPlanets)
+      .then(res => {
+        // console.log(res);
+        setPlanetsData(res.data);
+      })
+      .catch(err => console.err(err));
+    }
+    getAPI();
+  }, []);
+  
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {/* ❗ Map over the data in state, rendering a Character at each iteration */
+      peopleData.map((person) => {
+        return (
+            <Character key={person.id} peopleData={person} planetsData={planetsData}/>
+        )
+      })
+      }
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ peopleData, planetsData }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className='character-card'>
+      {/* Use the same markup with the same attributes as in the mock */
+        <h3 className='character-name'>{peopleData.name}</h3>
+      }
     </div>
   )
 }
